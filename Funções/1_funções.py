65a1258bd35a196634a05980f9e3065e8e8bf05b
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-09 11:51:10 -0300

    Funções e tuples

--- Funções/1_funções.py
+++ Funções/1_funções.py
@@ -1,6 +1,10 @@
 def calculo(x, y):
     resp = x * x + y * y
-    return resp
+    return resp, x, y
 
 resultado = calculo(10, 2)
 print(resultado)
+arroz, x, *_ =  calculo(10, 2)
+print(arroz)
+print(x)
+#[Listas], (Tuplas) = Não podem ser modificadas, {dicty}
