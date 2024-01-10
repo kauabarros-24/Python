94b11ca7c88d2f69c0d8e8ad46ee85adb3ff00ff
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-10 16:23:14 -0300

    tuplas

--- set e tuple/tuples.py
+++ set e tuple/tuples.py
@@ -1,6 +1,18 @@
 #A última forma básica de armazenar coleções de dados em Python é utilizando tuplas. Elas são parecidas com listas, porém, são imutáveis! Ou seja, uma vez criadas, não podem ser modificadas.
 # Além disso, é uma estrutura ordenada, isso é, os itens permanecem na ordem definida, e é permitido elementos duplicados, uma vez que uma tupla é indexada
 
-tupla = (1, 2)
-print(type(tupla)) # Imprime tuple
-tuple = (1)
+#tupla = (1, 2)
+#print(type(tupla)) # Imprime tuple
+#tuple = (1)
+
+#A, B, C = (1, 2), (1, 2), (2, 3)
+#print(A, B, C)
+#print(A > B)
+#print(A < B)
+#print(A == B)
+A = ("Kaua", "L", "M")
+dicionario = {
+    A[0]: "Eu",
+    A[1]: "Letra",
+    A[2]: "Outra letra"
+}
